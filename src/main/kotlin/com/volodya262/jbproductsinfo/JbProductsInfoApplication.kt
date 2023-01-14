1cb11c948412870ab6b7de231689647f57f37a56
Volodya262 <volodya_26@mail.ru>
2023-01-14 23:20:16 +0300

    initial commit

---
+++ src/main/kotlin/com/volodya262/jbproductsinfo/JbProductsInfoApplication.kt
@@ -0,0 +1,11 @@
+package com.volodya262.jbproductsinfo
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JbProductsInfoApplication
+
+fun main(args: Array<String>) {
+	runApplication<JbProductsInfoApplication>(*args)
+}
