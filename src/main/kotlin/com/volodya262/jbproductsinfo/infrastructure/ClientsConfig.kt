1142bdb1699aa5d7e3b3d2f7a47d0259d426dcb8
Volodya262 <volodya_26@mail.ru>
2023-01-23 23:26:34 +0300

    connection pool

--- src/main/kotlin/com/volodya262/jbproductsinfo/infrastructure/ClientsConfig.kt
+++ src/main/kotlin/com/volodya262/jbproductsinfo/infrastructure/ClientsConfig.kt
@@ -1,55 +1,81 @@
 package com.volodya262.jbproductsinfo.infrastructure
 
 import com.fasterxml.jackson.databind.ObjectMapper
+import org.apache.http.client.HttpClient
+import org.apache.http.impl.client.HttpClientBuilder
+import org.apache.http.impl.conn.PoolingHttpClientConnectionManager
+import org.springframework.beans.factory.annotation.Value
 import org.springframework.boot.web.client.RestTemplateBuilder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter
 import org.springframework.web.client.RestTemplate
+import java.time.Duration
 
 @Configuration
 class ClientsConfig(
-    val objectMapper: ObjectMapper
+    val objectMapper: ObjectMapper,
+    @Value("\${client.max-total-connections}")
+    val maxTotalConnections: Int,
+    @Value("\${client.default-max-per-route}")
+    val defaultMaxPerRoute: Int
 ) {
 
     @Bean
     fun jetBrainsUpdatesRestTemplate(): RestTemplate {
-        // TODO connections pool
-        val clientHttpRequestFactory = HttpComponentsClientHttpRequestFactory().apply {
-            setConnectTimeout(5000)
-            setConnectionRequestTimeout(5000)
-            setReadTimeout(5000)
+        val connectionManager = PoolingHttpClientConnectionManager().also {
+            it.maxTotal = maxTotalConnections
+            it.defaultMaxPerRoute = defaultMaxPerRoute
         }
 
-        return RestTemplate(clientHttpRequestFactory)
+        val httpClient: HttpClient = HttpClientBuilder.create()
+            .setConnectionManager(connectionManager)
+            .build()
+
+        return RestTemplateBuilder()
+            .setConnectTimeout(Duration.ofMillis(5000))
+            .setReadTimeout(Duration.ofMillis(5000))
+            .requestFactory { HttpComponentsClientHttpRequestFactory(httpClient) }
+            .build()
     }
 
     @Bean
     fun jetBrainsDataServicesRestTemplate(): RestTemplate {
-        // TODO connections pool
-        val clientHttpRequestFactory = HttpComponentsClientHttpRequestFactory().apply {
-            setConnectTimeout(5000)
-            setConnectionRequestTimeout(5000)
-            setReadTimeout(5000)
+        val connectionManager = PoolingHttpClientConnectionManager().also {
+            it.maxTotal = maxTotalConnections
+            it.defaultMaxPerRoute = defaultMaxPerRoute
         }
 
+        val httpClient: HttpClient = HttpClientBuilder.create()
+            .setConnectionManager(connectionManager)
+            .build()
+
         return RestTemplateBuilder()
+            .setConnectTimeout(Duration.ofMillis(5000))
+            .setReadTimeout(Duration.ofMillis(5000))
             .additionalMessageConverters(MappingJackson2HttpMessageConverter(objectMapper))
-            .requestFactory { clientHttpRequestFactory }
+            .requestFactory { HttpComponentsClientHttpRequestFactory(httpClient) }
             .build()
     }
 
     @Bean
     fun distributionsRestTemplate(): RestTemplate {
-        // TODO connections pool
-        val clientHttpRequestFactory = HttpComponentsClientHttpRequestFactory().apply {
-            setConnectTimeout(5000)
-            setConnectionRequestTimeout(5000)
-            setReadTimeout(5000)
-            setBufferRequestBody(false)
+        val connectionManager = PoolingHttpClientConnectionManager().also {
+            it.maxTotal = 10
+            it.defaultMaxPerRoute = 10
         }
 
-        return RestTemplate(clientHttpRequestFactory)
+        val httpClient: HttpClient = HttpClientBuilder.create()
+            .setConnectionManager(connectionManager)
+            .build()
+
+        return RestTemplateBuilder()
+            .setConnectTimeout(Duration.ofMillis(5000))
+            .setReadTimeout(Duration.ofMillis(5000))
+            .additionalMessageConverters(MappingJackson2HttpMessageConverter(objectMapper))
+            .requestFactory { HttpComponentsClientHttpRequestFactory(httpClient) }
+            .setBufferRequestBody(false)
+            .build()
     }
 }
